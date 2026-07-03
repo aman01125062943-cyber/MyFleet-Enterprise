c3706a8302ba9ea0d789f15865a5612d72bd83b5
Amin User <amin@example.com>
2026-07-03 14:45:28 +0300

    fix: substitute kong api keys at startup

---
+++ deploy/vps/supabase/kong-entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -eu
+
+awk '{
+  result = ""
+  rest = $0
+  while (match(rest, /\$[{]?[A-Za-z_][A-Za-z_0-9]*[}]?/)) {
+    token = substr(rest, RSTART, RLENGTH)
+    varname = token
+    sub(/^\$\{?/, "", varname)
+    sub(/\}?$/, "", varname)
+    if (varname in ENVIRON) {
+      result = result substr(rest, 1, RSTART - 1) ENVIRON[varname]
+    } else {
+      result = result substr(rest, 1, RSTART + RLENGTH - 1)
+    }
+    rest = substr(rest, RSTART + RLENGTH)
+  }
+  print result rest
+}' /home/kong/temp.yml > "$KONG_DECLARATIVE_CONFIG"
+
+exec /entrypoint.sh kong docker-start
